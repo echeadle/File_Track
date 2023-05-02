88341384adaa166f947e9b97531d7ae865996de5
echeadle <echeadle@gmail.com>
2023-05-01 19:35:40 -0600

    Add doc lists, modify file_check ignore more folders

--- file_check.py
+++ file_check.py
@@ -1,10 +1,12 @@
 import hashlib
 import os
 import re
+import shlex
+
 
 for folderName, subfolders, filenames in os.walk('/home/echeadle'):
     #print('The current folder is ' + folderName)
-    subfolders[:] = [f for f in subfolders if not f in ['VirtualBox VMs','anaconda3','snap','venv','temp']]
+    subfolders[:] = [f for f in subfolders if not f in ['ansible_collections','node_modules','google-cloud-sdk', 'go', 'VirtualBox VMs','anaconda3','snap','env','venv','temp']]
     subfolders[:] = [f for f in subfolders if not f.startswith('.')]
     subfolders[:] = [f for f in subfolders if not f.startswith('__')]
     
@@ -16,12 +18,12 @@ for folderName, subfolders, filenames in os.walk('/home/echeadle'):
     #filenames[:] = [f for f in filenames if  re.findSall('test', f, flags=re.IGNORECASE)]
     for filename in filenames:
         #print('FILE INSIDE ' + folderName + ': '+ filename)
-        full_file_path = os.path.join(folderName, f'"{filename}"')      
+        full_file_path = os.path.join(shlex.quote(folderName), shlex.quote(filename))
         the_hash = hashlib.sha256()   
         if os.path.isfile(full_file_path):
             with open(full_file_path, 'rb') as fn:
                 for line in fn.readlines():
                     the_hash.update(line)
             fn.close()
-            print(f'"{folderName}","{filename}","{the_hash.hexdigest()}"')
+            print(f'"{folderName}"  ,  "{filename}"  ,  "{the_hash.hexdigest()}"')
 
